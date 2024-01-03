9894c67249c290f30a18034bfaf1de71681c127e
JIA XIN <82912588+huchacha1314@users.noreply.github.com>
2024-01-03 14:29:55 +0100

    Update mlp_model.py
    add MLP层的注释

--- digit_depth/train/mlp_model.py
+++ digit_depth/train/mlp_model.py
@@ -1,4 +1,4 @@
-
+#3层 MLP 将RGB-> xyz法向量
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -10,10 +10,10 @@ class MLP(nn.Module):
             self, input_size=5, output_size=3, hidden_size=32):
         super().__init__()
 
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, hidden_size)
-        self.fc4 = nn.Linear(hidden_size, output_size)
+        self.fc1 = nn.Linear(input_size, hidden_size)#5RGBXY（XY在图像中的位置）->32 
+        self.fc2 = nn.Linear(hidden_size, hidden_size)#32->32
+        self.fc3 = nn.Linear(hidden_size, hidden_size)#32->32
+        self.fc4 = nn.Linear(hidden_size, output_size)#32->3 N_x N_y N_z的法向量
         self.drop = nn.Dropout(p=self.dropout_p)
 
     def forward(self, x):
