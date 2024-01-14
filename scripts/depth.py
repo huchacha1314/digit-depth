5189d14f95902a8e91be9b96b0d3bc757edf8aa4
JIA XIN <82912588+huchacha1314@users.noreply.github.com>
2024-01-14 10:56:38 +0100

    Update depth.py

--- scripts/depth.py
+++ scripts/depth.py
@@ -32,23 +32,46 @@ def show_depth(cfg):
     dm_zero_counter = 0
     dm_zero = 0
     while not rospy.is_shutdown():
-        #TODO
+        #TODO 
+        '''
+        ret, frame = self.__dev.read()
+        frame = cv2.transpose(frame, frame)
+        frame = cv2.flip(frame, 0)
+        一帧图像
+        '''
         #请确定 frame的格式
         frame = digit_call.get_frame()
         #TODO
+        '''
+        img_np=torch.tensor(df_base[['X', 'Y', 'R', 'G', 'B']].values, dtype=torch.float32).to(device)
+        img_np :(N, 5) N是一张图片内所有像素点的数量 320*240
+        '''
         img_np = preproc_mlp(frame)
         img_np = model(img_np).detach().cpu().numpy()
         #TODO
+        '''
+        img_np = img_np.reshape(320, 240, 3)
+        normal = copy.deepcopy(img_np)  # surface normal image
+        img_np = torch.tensor(img_np,
+                           dtype=torch.float32)  # convert to torch tensor for later processing in gradient computation
+        img_np = img_np.permute(2, 0, 1)  # swap axes to (3,320,240)
+        img_np = img_np # convert to uint8 for visualization
+        img_np and normal 的数据格式（3，320，240）
+        '''
         img_np, _ = post_proc_mlp(img_np)
         # get gradx and grady
+        # gradx_img grady_img (320,240)
         gradx_img, grady_img = geom_utils._normal_to_grad_depth(img_normal=img_np, gel_width=cfg.sensor.gel_width,
                                                                 gel_height=cfg.sensor.gel_height,bg_mask=None)
         # reconstruct depth
+        
         img_depth = geom_utils._integrate_grad_depth(gradx_img, grady_img, boundary=None, bg_mask=None,max_depth=cfg.max_depth)
+        # img_depth (320,240)
         # TODO
         img_depth = img_depth.detach().cpu().numpy() # final depth image for current image
         # Get the first 50 frames and average them to get the zero depth
         # 获取50张 depth background 的深度图，以确定0基准
+        # 建立 background 文件夹，存入50张图 采样背景background
         if dm_zero_counter < 50:
             dm_zero += img_depth
             dm_zero_counter += 1
@@ -59,6 +82,7 @@ def show_depth(cfg):
         
         # remove the zero depth
         # 将 再次生成的图片 减去 背景的深度值
+        # 建立force_pic 文件夹 ，将带有力度信息的图片存入
         diff = img_depth - dm_zero
         # convert pixels into 0-255 range
         diff = diff*255
